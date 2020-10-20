04141cd89a6bd3aec2fd198684b4c52febdd185c
Majid Hajian <mhadaily@gmail.com>
2020-10-20 15:54:15 +0200

    final weather app

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,511 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<WeatherInfo> fetchWeatherInfo() async {
+  final http.Response res = await http.get('https://jsonkeeper.com/b/OOJE');
+  if (res.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    final WeatherInfo result = WeatherInfo.fromJson(jsonDecode(res.body));
+    print(result);
+    return result;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class WeatherInfo {
+  final List<String> times;
+  final List<String> temps;
+  final List<String> days;
+
+  WeatherInfo({
+    this.times,
+    this.temps,
+    this.days,
+  });
+
+  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+    return WeatherInfo(
+      times: json['times'].cast<String>(),
+      temps: json['temps'].cast<String>(),
+      days: json['days'].cast<String>(),
+    );
+  }
+}
 
 void main() {
   runApp(MyApp());
 }
 
+class MyStatefulWidget extends StatefulWidget {
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with SingleTickerProviderStateMixin {
+  double opacityLevel = 1.0;
+  WeatherInfo weatherInfo;
+
+  AnimationController controller;
+  Animation<double> animation;
+  Animation<Offset> weatherIconOffsetAnimation;
+  Animation<Offset> dateOffsetAnimation;
+  Animation<Offset> weatherDescOffsetAnimation;
+  Animation<Offset> locationOffsetAnimation;
+  Animation<Offset> temperatureOffsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() async {
+      final WeatherInfo value = await fetchWeatherInfo();
+      setState(() {
+        weatherInfo = value;
+      });
+    });
+
+    controller = AnimationController(
+        duration: Duration(milliseconds: 2000), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    weatherIconOffsetAnimation = Tween<Offset>(
+      begin: Offset(0.0, 1.0),
+      end: Offset(0.0, 0.4),
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.0,
+          0.500,
+          curve: Curves.easeIn,
+        ),
+      ),
+    );
+    dateOffsetAnimation = Tween<Offset>(
+      begin: Offset(-0.5, 0.0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.0,
+          0.300,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+    weatherDescOffsetAnimation = Tween<Offset>(
+      begin: Offset(-0.5, 0.0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.1,
+          0.400,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+    locationOffsetAnimation = Tween<Offset>(
+      begin: Offset(-0.5, 0.0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.2,
+          0.600,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+    temperatureOffsetAnimation = Tween<Offset>(
+      begin: Offset(-0.5, 0.0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.4,
+          0.900,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Weather',
+          style: TextStyle(fontWeight: FontWeight.w300),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.pushNamed(context, '/setting');
+            },
+          ),
+        ],
+        backgroundColor: Colors.black,
+      ),
+      body: weatherInfo != null
+          ? Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.grey.shade900,
+                    Colors.black,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  _cloudIcon(),
+                  _temperature(),
+                  _location(),
+                  _date(),
+                  _weatherDesc(),
+                  _hourlyWeather(),
+                  _weeklyWeather(),
+                ],
+              ),
+            )
+          : Center(
+              child: Text('Loading...'),
+            ),
+    );
+  }
+
+  Widget _weeklyWeather() {
+    return Expanded(
+      child: FadeTransition(
+        opacity: animation,
+        child: ListView.builder(
+          itemCount: weatherInfo.days.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 30.0,
+              padding: EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      weatherInfo.days[index].toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Icon(
+                      Icons.cloud_queue,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        weatherInfo.temps[index].toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _hourlyWeather() {
+    return FadeTransition(
+      opacity: animation,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.white.withOpacity(0.3),
+            ),
+            bottom: BorderSide(
+              color: Colors.white.withOpacity(0.3),
+            ),
+          ),
+        ),
+        height: 100.0,
+        child: ListView.builder(
+          itemCount: weatherInfo.times.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              width: 50.0,
+              child: Card(
+                color: Colors.transparent,
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          weatherInfo.times[index].toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        Icon(Icons.cloud_queue, color: Colors.white),
+                        Text(
+                          weatherInfo.temps[index].toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _cloudIcon() {
+    return Container(
+      height: 200.0,
+      child: Center(
+        child: SlideTransition(
+          position: weatherIconOffsetAnimation,
+          child: FadeTransition(
+            opacity: animation,
+            child: Image.asset(
+              'assets/images/snow.png',
+              height: 90.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _temperature() {
+    return SlideTransition(
+      position: temperatureOffsetAnimation,
+      child: FadeTransition(
+        opacity: animation,
+        child: Container(
+          padding: EdgeInsets.only(left: 20.0),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            '-10\u00B0',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 84.0,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _location() {
+    return SlideTransition(
+      position: locationOffsetAnimation,
+      child: FadeTransition(
+        opacity: animation,
+        child: Container(
+          padding: EdgeInsets.only(left: 20.0),
+          alignment: Alignment.centerLeft,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.place, color: Colors.white),
+              Text(
+                'London, UK',
+                style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _date() {
+    return SlideTransition(
+      position: dateOffsetAnimation,
+      child: FadeTransition(
+        opacity: animation,
+        child: Container(
+          padding: EdgeInsets.only(left: 20.0, top: 10.0),
+          alignment: Alignment.centerLeft,
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  'TODAY',
+                  style: TextStyle(
+                      color: Colors.grey[700],
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14.0),
+                ),
+              ),
+              Text(
+                '20.10.2020',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _weatherDesc() {
+    return SlideTransition(
+      position: weatherDescOffsetAnimation,
+      child: FadeTransition(
+        opacity: animation,
+        child: Container(
+          padding: EdgeInsets.only(left: 20.0, bottom: 20.0),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Snow turn to cloud',
+            style: TextStyle(
+              color: Colors.grey[500],
+              fontSize: 16.0,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/setting': (context) => SettingScreen(),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.grey,
+                        Colors.black,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: Center(
+                    child: MyStatefulWidget(),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SettingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.grey.shade900,
+              Colors.black,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: ElevatedButton(
+            child: Text('Go to Homescreen'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
     );
   }
 }
